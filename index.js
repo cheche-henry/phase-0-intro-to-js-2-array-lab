128ac27f890b64dba119ce0bb136a3bd6e4cd223
henry cheche <henry.cheche@student.moringaschool.com>
2023-03-15 14:13:09 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'] 
+
+function destructivelyAppendCat(name) {
+   cats.push(name);
+   return cats;
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+  return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+  return cats;
+}
+
+function appendCat(name){
+  const random1= [...cats,name];
+  return random1;
+}
+
+function prependCat(name){
+  const random2=[name, ...cats];
+  return random2
+}
+
+function removeLastCat(){
+  const cats1= cats.slice(0,cats.length-1);
+  return cats1;
+}
+
+function removeFirstCat(){
+    var cats2= cats.slice(1)
+    return cats2
+  }
